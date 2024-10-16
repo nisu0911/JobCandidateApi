a7e40be724ae980cb321afcfa8628381869d24c7
nisu0911 <shakya.nischal0911@gmail.com>
2024-10-16 20:01:45 +0545

    Add project files.

---
+++ JobCandidateApi/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure;
+
+namespace JobCandidateApi
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddInfrastructureServices();
+            return services;
+        }
+    }
+}

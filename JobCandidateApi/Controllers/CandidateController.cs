80ebe568a403a5e0c4695e55574676adab434f3b
nisu0911 <shakya.nischal0911@gmail.com>
2024-10-16 21:40:27 +0545

    Adds api for upsert action

---
+++ JobCandidateApi/Controllers/CandidateController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobCandidateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidateController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost("upsert")]
+        public async Task<IActionResult> UpsertAsync(Candidate candidate, CancellationToken cancellationToken)
+        {
+            var result = await _candidateService.UpsertAsync(candidate, cancellationToken);
+
+            return result ? Ok() : StatusCode(500, "Error during upsert operation.");
+        }
+    }
+}

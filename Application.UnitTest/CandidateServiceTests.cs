75d576beb46151de9bb4031aff43d1f62b9fdd5f
nisu0911 <shakya.nischal0911@gmail.com>
2024-10-18 09:23:20 +0545

    Adds unit tests

---
+++ Application.UnitTest/CandidateServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using Application.Services;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Application.UnitTests
+{
+    [TestFixture]
+    public class CandidateServiceTests
+    {
+        private Mock<ICandidateRepository> _candidateRepoMock;
+        private Mock<ILogger<CandidateService>> _loggerMock;
+        private CandidateService _candidateService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _candidateRepoMock = new Mock<ICandidateRepository>();
+            _loggerMock = new Mock<ILogger<CandidateService>>();
+            _candidateService = new CandidateService(_candidateRepoMock.Object, _loggerMock.Object);
+        }
+
+        [Test]
+        public async Task UpsertCandidate_CallsRepositoryMethod()
+        {
+            var cancellationToken = new CancellationToken();
+
+            // Arrange
+            var candidate = new Candidate
+            { 
+                Email = "test@test.com",
+                FirstName = "test",
+                LastName = "test",
+                PhoneNumber = "1234567890",
+                TimeInterval = "test",
+                Github = "test",
+                Linkedin = "test",
+                Comment = "test"
+            };
+
+            // Act
+            await _candidateService.UpsertAsync(candidate, cancellationToken);
+
+            // Assert
+            _candidateRepoMock.Verify(repo => repo.UpsertAsync(candidate, cancellationToken), Times.Once);
+        }
+
+        [Test]
+        public void UpsertCandidate_ThrowsApplicationException_OnDbUpdateException()
+        {
+            var cancellationToken = new CancellationToken();
+
+            // Arrange
+            var candidate = new Candidate
+            {
+                Email = "test@test.com",
+                FirstName = "test",
+                LastName = "test",
+                PhoneNumber = "1234567890",
+                TimeInterval = "test",
+                Github = "test",
+                Linkedin = "test",
+                Comment = "test"
+            };
+
+            _candidateRepoMock
+                .Setup(repo => repo.UpsertAsync(candidate, cancellationToken))
+                .ThrowsAsync(new DbUpdateException());
+
+            // Act & Assert
+            var exception = Assert.ThrowsAsync<ApplicationException>(() => _candidateService.UpsertAsync(candidate, cancellationToken));
+            Assert.That(exception.Message, Does.Contain("Could not upsert candidate."));
+        }
+    }
+}
---
+++ Infrastructure.UnitTests/CandidateRepositoryTests.cs
@@ -0,0 +1,120 @@
+﻿
+using Core.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Infrastructure.UnitTests
+{
+    [TestFixture]
+    public class CandidateRepositoryTests
+    {
+        private Mock<ILogger<CandidateRepository>> _loggerMock;
+        private JobCandidateDbContext _context;
+        private CandidateRepository _candidateRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Set up an in-memory database for testing
+            var options = new DbContextOptionsBuilder<JobCandidateDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new JobCandidateDbContext(options);
+            _loggerMock = new Mock<ILogger<CandidateRepository>>();
+            _candidateRepository = new CandidateRepository(_context, _loggerMock.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task UpsertCandidate_InsertsNewCandidate()
+        {
+            var cancellationToken = new CancellationToken();
+            
+            // Arrange
+            var newCandidate = new Candidate
+            {
+                Email = "test@test.com",
+                FirstName = "test",
+                LastName = "test",
+                PhoneNumber = "1234567890",
+                TimeInterval = "test",
+                Github = "test",
+                Linkedin = "test",
+                Comment = "test"
+            };
+
+            // Act
+            await _candidateRepository.UpsertAsync(newCandidate, cancellationToken);
+
+            // Assert
+            var candidate = await _context.Candidates.FindAsync(newCandidate.Email);
+            Assert.IsNotNull(candidate);
+            Assert.That(candidate.Email, Is.EqualTo(newCandidate.Email));
+        }
+
+        [Test]
+        public async Task UpsertCandidate_UpdatesExistingCandidate()
+        {
+            var cancellationToken = new CancellationToken();
+            
+            // Arrange
+            var existingCandidate = new Candidate
+            {
+                Email = "test@test.com",
+                FirstName = "test",
+                LastName = "test",
+                PhoneNumber = "1234567890",
+                TimeInterval = "test",
+                Github = "test",
+                Linkedin = "test",
+                Comment = "test"
+            };
+
+            _context.Candidates.Add(existingCandidate);
+            await _context.SaveChangesAsync();
+
+            var updatedCandidate = new Candidate
+            {
+                Email = "test@test.com",
+                FirstName = "test1",
+                LastName = "test1",
+                PhoneNumber = "1234567890",
+                TimeInterval = "test",
+                Github = "test",
+                Linkedin = "test",
+                Comment = "test"
+            };
+
+            // Act
+            await _candidateRepository.UpsertAsync(updatedCandidate, cancellationToken);
+
+            // Assert
+            var candidate = await _context.Candidates.FindAsync(updatedCandidate.Email);
+            Assert.That(candidate, Is.Not.Null);
+            Assert.That(candidate.Email, Is.EqualTo(updatedCandidate.Email));
+        }
+
+        [Test]
+        public void UpsertCandidate_ThrowsDbUpdateException()
+        {
+            var cancellationToken = new CancellationToken();
+            
+            // Arrange
+            var candidateRepo = new CandidateRepository(_context, _loggerMock.Object);
+            var invalidCandidate = new Candidate { Email = "test@test.com" };
+
+            // Act & Assert
+            Assert.ThrowsAsync<DbUpdateException>(async() => await candidateRepo.UpsertAsync(invalidCandidate, cancellationToken));
+        }
+    }
+}
